4f008ff713ca1e7fbaeda68f861ec39eb88ef9ad
Mario Pohrib <m9@Mario-MacBook-Air.local>
2025-11-29 19:33:11 +0100

    update

--- backend/api.py
+++ backend/api.py
@@ -39,5 +39,5 @@ async def post(text:dict):
     
   
 
-if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
+#if __name__ == "__main__":
+    #uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
